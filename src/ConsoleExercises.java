9175f4453aea11e3827036f75a6f4193fe1c454d
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-09-17 13:34:22 -0500

    exercise after walkthrough

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,37 +4,55 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
 //        double pi = 3.14159;
-//        System.out.format("The value of pi is approximatley %-10.2f%n  \n",
+//        System.out.format("The value of pi is approximatley %.2f.\n",
 //        pi);
-////        cant get period on same line
+
 //
+/**
+ * double is ok for small numbers
+ * printf is like format (its the newest version)
+ *% (is being rounded)
+ *
+ */
 //
 //        Scanner input = new Scanner(System.in);
 //        System.out.println("Enter a number: ");
 //        int userNumber = input.nextInt();
 //        System.out.println("You entered: --> \"" + userNumber + "\" <--");
-//
+
 //        Scanner user = new Scanner(System.in);
 //        System.out.println("Enter three words: ");
-//        String UserInput1 = user.next();
-//        String UserInput2 = user.next();
-//        String UserInput3 = user.next();
+//        String UserInput1 = user.nextLine();
+//        String UserInput2 = user.nextLine();
+//        String UserInput3 = user.nextLine();
 //        System.out.println(UserInput1);
 //        System.out.println(UserInput2);
 //        System.out.println(UserInput3);
+//        another solution!!
+
+//        System.out.format("%s%n %s%n %s%n", UserInput1, UserInput2, UserInput3);
 //
 ////        if you enter more than three words it prints them on the same line ®
 //
-
+/**
+ * you CAN use scanner only once on a method
+ * to test your own code validate ex aNum == 45
+ * should return TRUE else False
+ * the main goal is to accomplish the solution dont be overcomplicated
+ */
+//
 //        Scanner userInput = new Scanner(System.in);
-////        System.out.println("Enter a sentence");
-////        String userSentence = userInput.nextLine();
-////        System.out.println("Your sentence is: " + userSentence );
+//        System.out.println("Enter a sentence");
+//        String userSentence = userInput.nextLine();
+//        System.out.println("Your sentence is: " + userSentence );
+/**
+ * dont use next unless you want to grab things by spaces.
+ */
 
 //        Scanner studentInput = new Scanner(System.in);
 //        System.out.println("Enter the Length of the classroom: ");
 //        String input1= studentInput.nextLine();
-//        int length = Integer.parseInt(input1);
+//        int length = Integer.parseInt(studentInput.nextLine(input1)); ****
 //
 //
 //        System.out.println("Enter the Width of the classroom: ");
@@ -47,21 +65,25 @@ public class ConsoleExercises {
 //        int perimeter = (length *2) + (width * 2);
 //        System.out.println("The perimeter of the classroom is: " + perimeter);
 
-        Scanner studentInput = new Scanner(System.in);
-        System.out.println("Enter the Length of the classroom: ");
-        String input1= studentInput.nextLine();
-        double length = Double.parseDouble(input1);
-
-
-        System.out.println("Enter the Width of the classroom: ");
-        String input2 = studentInput.next();
-        double width = Double.parseDouble(input2);
-
-        double area = length*width;
-        System.out.println("The area of the classroom is: "+ area);
-
-        double perimeter = (length * 2) + (width * 2);
-        System.out.println("The perimeter of the classroom is: " + perimeter);
-
+//        Scanner studentInput = new Scanner(System.in);
+//        System.out.println("Enter the Length of the classroom: ");
+//        String input1= studentInput.nextLine();
+//        double length = Double.parseDouble(input1);
+//
+//
+//        System.out.println("Enter the Width of the classroom: ");
+//        String input2 = studentInput.next();
+//        double width = Double.parseDouble(input2);
+//
+//        double area = length*width;
+//        System.out.println("The area of the classroom is: "+ area);
+//
+//        double perimeter = (length * 2) + (width * 2);
+//        System.out.println("The perimeter of the classroom is: " + perimeter);
+/**
+ *  soutv gives you back the sout plus the string
+ *  instead of parseInt you can use valueOf
+ *
+ */
     }
 }
