74f1abab27870cb6fbd29b13692b8ebb6ec4cf85
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-09-17 08:27:07 -0500

    sept 16

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,81 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World");
+//        System.out.print("Hello, World");
+
+        int myFavoriteNumber = 3;
+        System.out.println(myFavoriteNumber);
+
+      String  myString = "this is a string";
+        System.out.println(myString);
+
+
+
+//        if you change it to ' singles ' it does not work
+//        will not work if you input a number it is incompatible type
+//
+//        long myNumber = ;
+//        System.out.println(myNumber);
+//        illegal start of expression
+
+//        long myNumber = 3.14;
+//        System.out.println(myNumber);
+//        incompatible types
+
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+//there is a decimal? if it is 3.14 it does not work
+
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+
+        float  myNumber = 3.14F;
+        System.out.println(myNumber);
+
+//        CORRECT
+//        float will not work must be changed to double
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        class x = 4;
+//        System.out.println(x);
+//        this does not work and I dont know why???
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        idk what happened...three is not a number it is a string
+//        int three = (int) "three";
+
+//        int x = 4 ;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//
+//        System.out.println(x);
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+        short m = -4351;
+        System.out.println(m);
+
+
+
+
 
     }
 }
