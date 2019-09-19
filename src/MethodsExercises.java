c406ac163ba847958b496e7b6f2a2188dd168625
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-09-19 10:43:52 -0500

    addition and subtraction

---
+++ src/MethodsExercises.java
@@ -0,0 +1,14 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        System.out.println(addition(1, 3));
+        System.out.println(subtraction(5,2));
+    }
+
+    public static int addition(int num1, int num2){
+        return num1 + num2;
+    }
+    public static int subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+    public static int
+}
