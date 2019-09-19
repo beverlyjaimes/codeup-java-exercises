554f9308ebcabb54bc3e06ebe670ff7889a7530b
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-09-19 10:45:24 -0500

    multiplication

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,14 +1,17 @@
 public class MethodsExercises {
     public static void main(String[] args) {
-        System.out.println(addition(1, 3));
+        System.out.println(addition(5, 2));
         System.out.println(subtraction(5,2));
+        System.out.println(multiplication(5,2));
     }
 
-    public static int addition(int num1, int num2){
+    public static int addition(int num1, int num2) {
         return num1 + num2;
     }
     public static int subtraction(int num1, int num2) {
         return num1 - num2;
     }
-    public static int
+    public static int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
 }
