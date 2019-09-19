30df4bf4c97cc0439081f3b67df3a89abd71a108
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-09-19 10:48:04 -0500

    division

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -3,6 +3,7 @@ public class MethodsExercises {
         System.out.println(addition(5, 2));
         System.out.println(subtraction(5,2));
         System.out.println(multiplication(5,2));
+        System.out.println(division(10,2));
     }
 
     public static int addition(int num1, int num2) {
@@ -13,5 +14,8 @@ public class MethodsExercises {
     }
     public static int multiplication(int num1, int num2) {
         return num1 * num2;
+    }
+    public static int division(int num1, int num2) {
+        return num1/num2;
     }
 }
