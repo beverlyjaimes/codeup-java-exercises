7926681c13f2ee38e1b1f6bffe6655e47204c6eb
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-09-25 11:00:44 -0500

    updated with fernandos examples

--- src/util/Input.java
+++ src/util/Input.java
@@ -78,27 +78,39 @@ public class Input {
         return scanner.nextInt();
     }
 
+
     public double getDouble(double min, double max) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number between 1 and 10: ");
-        if (input.hasNextDouble()) {
-            int numInput = input.nextInt();
-            if (numInput >= min && numInput <= max) {
-                System.out.println(numInput + " is between 1 & 10!");
-            } else if (numInput < min || numInput > max) {
-                System.out.println("This number is not valid, enter another number: ");
-                return getDouble(min, max);
-            }
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter a number between 1 and 10: ");
+//        if (input.hasNextDouble()) {
+//            int numInput = input.nextInt();
+//            if (numInput >= min && numInput <= max) {
+//                System.out.println(numInput + " is between 1 & 10!");
+//            } else if (numInput < min || numInput > max) {
+//                System.out.println("This number is not valid, enter another number: ");
+//                return getDouble(min, max);
+//            }
+//        } else {
+//            System.out.println("Not a valid input! Enter a valid number: ");
+//            return getDouble(min, max);
+//        }
+//        return getDouble(min, max);
+        /**
+         *FERNANDOS WAY
+         */
+        System.out.println("Enter a decimal:");
+        Double number = Double.parseDouble(this.scanner.nextLine());
+        if(number >=min && number <= max) {
+            return number;
         } else {
-            System.out.println("Not a valid input! Enter a valid number: ");
-            return getDouble(min, max);
+            System.out.println("Out of range");
         }
         return getDouble(min, max);
 
     }
 
     public int getDouble() {
-        System.out.println("Enter a number with a decimal:");
+        System.out.println("Enter a decimal:");
         return scanner.nextInt();
     }
 
