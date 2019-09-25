b99e79205fd522b9e19249459de077f923d6404e
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-09-25 16:29:49 -0500

    3 step 2/8

---
+++ src/movies/Movie.java
@@ -0,0 +1,23 @@
+package movies;
+
+public class Movie {
+    private String name;
+    private String category;
+
+    public String getName() {
+        return name;
+
+    }
+    public void changeName() {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+
+    }
+    public void changeCategory() {
+        this.category = category;
+    }
+}
+
