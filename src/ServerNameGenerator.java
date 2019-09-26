a054b9d5ca971e303bf501aed657cbd2dc182691
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-09-26 10:35:32 -0500

    corrected 2 to be dynamic

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -3,16 +3,19 @@ public class ServerNameGenerator {
     static String[] adjectives = {"Bitter", "Fluffy", "Fuzzy", "Sharp", "Soft", "Quiet", "Loud", "Yellow", "Little", "Small"};
     static String[] nouns = {"Lion", "Mother", "Baby", "Puppy", "Computer", "Dragon", "Dog", "Monster", "Woman", "Student"};
 
+
+    public static String getRandomEle(String[] arr){
+        int index = (int)(Math.random() * ((arr.length - 1) + 1));
+        return arr[index];
+    }
     public static void main(String[] args) {
-        random();
 
-    }
-    public static int getRandom(int max){
-        return(int) (Math.random()*max);
-    }
-    public static void random(){
         System.out.println("Here is your server name: ");
-        System.out.println(adjectives[getRandom(10)] + " " + nouns[getRandom(10)]);
+//        System.out.println(adjectives[getRandom(10)] + " " + nouns[getRandom(10)]);
+        String message = String.format("%s %s", getRandomEle(adjectives), getRandomEle(nouns));
+        System.out.println(message);
 
     }
+
+
 }
