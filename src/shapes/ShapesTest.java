dedecc10701286677e538e23c60c7231905c5c83
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-09-29 15:46:58 -0500

    testing

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,26 +3,26 @@ package shapes;
 public class ShapesTest {
     public static void main(String[] args) {
 
-//        Rectangle box1 = new Rectangle(5,4);
-//        System.out.println(box1.getPerimeter());
-//        System.out.println(box1.getArea());
-//
-//        Rectangle box2 = new Square(5);
-//        System.out.println(box2.getPerimeter());
-//        System.out.println(box2.getArea());
-//
-//        Square box3 = new Square(6);
-//        System.out.println(box3.getPerimeter());
-//        System.out.println(box3.getArea());
-//    }
-        Measurable myShape;
-        myShape = new Rectangle(5, 4);
-        System.out.println(myShape.getArea());
-        System.out.println(myShape.getPerimeter());
+        Rectangle box1 = new Rectangle(5,4);
+        System.out.println(box1.getPerimeter());
+        System.out.println(box1.getArea());
 
-        myShape = new Square( 5);
-        System.out.println(myShape.getArea());
-        System.out.println(myShape.getPerimeter());
+        Rectangle box2 = new Square(5);
+        System.out.println(box2.getPerimeter());
+        System.out.println(box2.getArea());
 
+        Square box3 = new Square(6);
+        System.out.println(box3.getPerimeter());
+        System.out.println(box3.getArea());
     }
+//        Measurable myShape;
+//        myShape = new Rectangle(5, 4);
+//        System.out.println(myShape.getArea());
+//        System.out.println(myShape.getPerimeter());
+//
+//        myShape = new Square( 5);
+//        System.out.println(myShape.getArea());
+//        System.out.println(myShape.getPerimeter());
+
+//    }
 }
